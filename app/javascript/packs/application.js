cf47358de8b00d62b1bece505d16981a5a744e1b
khemlall <khemlall.mangal@gmail.com>
2021-02-02 09:23:36 -0500

    setting up javascript with webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,7 @@
-import 'stylesheets/application'
-import 'material-dashboard.js'
+import '../stylesheets/application';
+//import 'material-dashboard.js';
+import 'bootstrap';
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+});
