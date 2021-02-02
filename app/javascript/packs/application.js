e5705d7b2300d7efe5101445dad48da87fb8b859
khemlall <khemlall.mangal@gmail.com>
2021-02-02 16:22:55 -0500

    adding this new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,13 @@
 import '../stylesheets/application';
 //import 'material-dashboard.js';
 import 'bootstrap';
-
+import 'material/bootstrap-material-design';
+import 'material/perfect-scrollbar.jquery.min';
+import 'material/chartist.min';
+import 'material/arrive.min';
+import 'material/bootstrap-notify';
+import 'material/material-dashboard';
+import 'material/demo';
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
