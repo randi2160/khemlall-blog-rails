a184e10b063678542b7671a9592c9b1a501b9155
khemlall <khemlall.mangal@gmail.com>
2021-02-02 16:39:16 -0500

    untrack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,14 @@
 import '../stylesheets/application';
 //import 'material-dashboard.js';
 import 'bootstrap';
-import 'material-dashboard.js'
-import 'material/bootstrap-material-design';
-import 'material/perfect-scrollbar.jquery.min';
-import 'material/chartist.min';
-import 'material/arrive.min';
-import 'material/bootstrap-notify';
-import 'material/material-dashboard';
-import 'material/demo';
+//import '../packs/material-dashboard.js'
+//import 'material/bootstrap-material-design';
+//import 'material/perfect-scrollbar.jquery.min';
+//import 'material/chartist.min';
+//import 'material/arrive.min';
+//import 'material/bootstrap-notify';
+//import 'material/material-dashboard';
+//import 'material/demo';
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
